04e36141f79fbd732011f4eb069f3dd1fb15bdd3
Jose Antonio Camiña <jose.camina@lana.xyz>
2020-03-20 18:20:52 +0100

    remove unused props in test

--- src/buttons/FigureCard/FigureCard.test.js
+++ src/buttons/FigureCard/FigureCard.test.js
@@ -8,13 +8,11 @@ describe('FigureCard unit test:', () => {
 		className: '',
 		meta: 'Meta text',
 		title: 'Title text',
-		imageSrc: 'myImage.png',
-		link: '/redirect/to/here',
-		onClick: () => {},
+		imageSrc: 'myImage.png'
 	}
 	it('Should set imageSrc as background for figure-card-test-image:', () => {
 		const mockedClick = jest.fn()
-		const wrapper = mount(<FigureCard {...defaultProps} onClick={mockedClick} />)
+		const wrapper = mount(<FigureCard {...defaultProps} />)
 		const figureStyles = wrapper.find('[data-testid="figure-card-test-image"]').prop('style')
 		expect(figureStyles).toHaveProperty('backgroundImage', "url('myImage.png')")
 	})
