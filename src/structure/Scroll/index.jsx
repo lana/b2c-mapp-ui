b2f5341d25d167ff2b2e9b9064261a3484f65859
jantocv <jose.camina@lana.xyz>
2020-04-28 13:19:32 +0200

    Remove no necessary css class

--- src/structure/Scroll/index.jsx
+++ src/structure/Scroll/index.jsx
@@ -20,7 +20,7 @@ export default class Scroll extends Component {
 				ref={elem => (this.node = elem || null)}
 				className={`${CSS.scroll} ${this.props.className || ''}`}
 			>
-				<div className={CSS.scrollableContent}> {this.props.children} </div>
+				<div> {this.props.children} </div>
 			</section>
 		)
 	}
