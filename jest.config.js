44badddce26d0169772659bd022221a1aba623d2
Jason Dujardin-Terry <jason.dujardin@lana.xyz>
2020-04-29 11:58:52 +0200

    Ported libs over from the old utils directory

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  setupFiles: ['<rootDir>/setupTests.js'],
   testMatch: [
     '**/*.test.js',
     '**/*.spec.js',
   ],
+  verbose: true,
 };
