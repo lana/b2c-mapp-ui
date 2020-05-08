ad6fac6f2983b97365884b880c4989a2f91ca363
Jason Dujardin-Terry <jason.dujardin@lana.xyz>
2020-05-08 11:47:24 +0200

    Updated Icons story to add a link to the design assets repo

--- src/components/Icons/Icons.stories.js
+++ src/components/Icons/Icons.stories.js
@@ -16,7 +16,7 @@ const defaultExample = () => ({
   template: `
     <div style="margin: 10px 50px 10px 50px;">
       <h2><strong>Icons <small>({{ availableIconNames.length }})</small>:</strong>&nbsp;A gallery of all the available icons.</h2>
-      <p style="margin-top: 10px;"><em>NOTE</em>: the icons are maintained in the following repo: <code>@lana/b2c-mapp-ui-assets</code></p>
+      <p style="margin-top: 10px;"><em>NOTE</em>: the icons are maintained in the following repo: <a target="_blank" href="https://github.com/lana/b2c-mapp-ui-assets/"><code>@lana/b2c-mapp-ui-assets</code></a></p>
       <hr>
       <div style="margin-top: 20px; display: flex; flex-direction: column;">
         <template v-for="(icon, index) in availableIconNames">
