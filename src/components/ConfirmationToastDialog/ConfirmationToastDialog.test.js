e70213177212d45ded65409cf7899127a93c44ec
jantocv <jose.camina@lana.xyz>
2020-05-12 16:18:34 +0200

    ConfirmationToastDialog unit test

--- src/components/ConfirmationToastDialog/ConfirmationToastDialog.test.js
+++ src/components/ConfirmationToastDialog/ConfirmationToastDialog.test.js
@@ -14,7 +14,7 @@ describe('UI/buttons/Button', () => {
     title: 'title',
     description: 'description',
     confirmButtonText: 'confirm',
-    dismissButtonText: 'dismiss',
+    secondaryButtonText: 'secondary',
     disabled: false,
   };
 
@@ -61,12 +61,12 @@ describe('UI/buttons/Button', () => {
     const childrenIsNotShown = !queryAllByTestId('confirmation-modal-dialog-slot').length;
     expect(childrenIsNotShown).toBeTruthy();
   });
-  //
-  // it('Should show given children if description is not provided', () => {
-  //   const { getByTestId } = render(ConfirmationToastDialogWrapper);
-  //   const childrenIsShown = getByTestId('confirmation-modal-dialog-slot').textContent.includes('Hey!');
-  //   expect(childrenIsShown).toBeTruthy();
-  // });
+
+  it('Should show given children if description is not provided', () => {
+    const { getByTestId } = render(ConfirmationToastDialog, { slots: { default: '<span data-testid="confirmation-modal-dialog-slot">Hey!</span>' }, propsData: { ...defaultProps, description: null } });
+    const childrenIsShown = getByTestId('confirmation-modal-dialog-slot').textContent.includes('Hey!');
+    expect(childrenIsShown).toBeTruthy();
+  });
 
   describe('Confirm actions behavior', () => {
     it('Should show action-confirm button when confirm is given', () => {
@@ -93,134 +93,28 @@ describe('UI/buttons/Button', () => {
     });
   });
 
-  // describe('Secondary actions behavior', () => {
-  //   it('Should show action-dismiss button when dismiss is given', () => {
-  //     const { getByTestId } = render(ConfirmationToastDialog, { propsData: { ...defaultProps } });
-  //     const dismissIsShown = getByTestId('bottom-dialog-action-dismiss-button').textContent.includes('dismiss');
-  //     expect(dismissIsShown).toBeTruthy();
-  //   });
-  //
-  //   it('Should NOT show action-dismiss button when dismiss is NOT given', () => {
-  //     const { queryAllByTestId } = render(ConfirmationToastDialog, { propsData: {
-  //       ...defaultProps,
-  //       dismissButtonText: null,
-  //     } });
-  //     const dismissIsNotShown = !queryAllByTestId('bottom-dialog-action-dismiss-button').length;
-  //     expect(dismissIsNotShown).toBeTruthy();
-  //   });
-  //
-  //   it('Should emit an event when dismiss is given and action-dismiss is clicked', () => {
-  //     const { getByTestId, emitted } = render(ConfirmationToastDialog, { propsData: { ...defaultProps } });
-  //     const dismissCTA = getByTestId('bottom-dialog-action-dismiss-button');
-  //     fireEvent.click(dismissCTA);
-  //     const clickEvent = emitted().dismiss;
-  //     expect(clickEvent).toBeTruthy();
-  //   });
-  // });
+  describe('Secondary actions behavior', () => {
+    it('Should show bottom-dialog-action-secondary-button button when secondaryText is given', () => {
+      const { getByTestId } = render(ConfirmationToastDialog, { propsData: { ...defaultProps } });
+      const dismissIsShown = getByTestId('bottom-dialog-action-secondary-button').textContent.includes('secondary');
+      expect(dismissIsShown).toBeTruthy();
+    });
+
+    it('Should NOT show bottom-dialog-action-secondary-button when secondaryText is NOT given', () => {
+      const { queryAllByTestId } = render(ConfirmationToastDialog, { propsData: {
+        ...defaultProps,
+        secondaryButtonText: null,
+      } });
+      const secondaryIsNotShown = !queryAllByTestId('bottom-dialog-action-secondary-button').length;
+      expect(secondaryIsNotShown).toBeTruthy();
+    });
+
+    it('Should emit an event when secondary is given and bottom-dialog-action-secondary-button is clicked', () => {
+      const { getByTestId, emitted } = render(ConfirmationToastDialog, { propsData: { ...defaultProps } });
+      const secondaryCTA = getByTestId('bottom-dialog-action-secondary-button');
+      fireEvent.click(secondaryCTA);
+      const clickEvent = emitted().secondary;
+      expect(clickEvent).toBeTruthy();
+    });
+  });
 });
-// TODO: Uncomment the below test cases after refactoring to work with Vue
-// describe('UI/overlays/ConfirmationToastDialog', () => {
-//   it('Should show description if provided', () => {
-//     const { getByTestId } = render(<ConfirmationToastDialog description={'description'} />);
-//     const descriptionIsShown = getByTestId('bottom-dialog-description').textContent === 'description';
-//     expect(descriptionIsShown).toBeTruthy();
-//   });
-//
-//   it('Should show children if provided', () => {
-//     const { getByTestId } = render(<ConfirmationToastDialog children={'children'} />);
-//     const childrenIsShown = getByTestId('bottom-dialog-children').textContent === 'children';
-//     expect(childrenIsShown).toBeTruthy();
-//   });
-//
-//   it('Should not show children if also description is given', () => {
-//     const { queryAllByTestId } = render(<ConfirmationToastDialog children={'children'} description="description" />);
-//     const childrenIsNotShown = !queryAllByTestId('bottom-dialog-children').length
-//     expect(childrenIsNotShown).toBeTruthy();
-//   })
-//
-//   it('Should show title if provided', () => {
-//     const { getByTestId } = render(<ConfirmationToastDialog title={'title'} />);
-//     const titleIsShown = getByTestId('bottom-dialog-title').textContent === 'title';
-//     expect(titleIsShown).toBeTruthy();
-//   });
-//
-//   it('Should call given onDismiss if dismiss is clicked', () => {
-//     const mockDismiss = jest.fn();
-//     const { getByTestId } = render(<ConfirmationToastDialog onDismiss={mockDismiss} />);
-//     const dismissCTA = getByTestId('bottom-dialog-dismiss');
-//     fireEvent.click(dismissCTA);
-//     expect(mockDismiss).toHaveBeenCalled();
-//   });
-//
-//   it('Should apply visible class if visible is given:', () => {
-//     const { getByTestId } = render(<ConfirmationToastDialog visible />);
-//     const isVisible = getByTestId('bottom-dialog-section').className.includes('visible');
-//     expect(isVisible).toBeTruthy();
-//   })
-//
-//   it('Should NOT visible class if visible is given to false:', () => {
-//     const { getByTestId } = render(<ConfirmationToastDialog visible={false} />);
-//     const isNotVisible = !getByTestId('bottom-dialog-section').className.includes('visible');
-//     expect(isNotVisible).toBeTruthy();
-//   })
-//
-//   describe('confirm actions behavior', () => {
-//     it('Should show action-confirm button when confirm is given', () => {
-//       const { getByTestId } = render(<ConfirmationToastDialog confirm={'confirm'} />);
-//       const confirmIsShown = getByTestId('bottom-dialog-action-confirm-button').textContent === 'confirm';
-//       expect(confirmIsShown).toBeTruthy();
-//     });
-//
-//     it('Should NOT show action-confirm button when confirm is NOT given', () => {
-//       const { queryAllByTestId } = render(<ConfirmationToastDialog />);
-//       const confirmIsNotShown = !queryAllByTestId('bottom-dialog-action-confirm-button').length;
-//       expect(confirmIsNotShown).toBeTruthy();
-//     });
-//
-//     it('Should call on confirm when confirm is given and action-confirm is clicked', () => {
-//       const mockConfirm = jest.fn();
-//       const { getByTestId } = render(<ConfirmationToastDialog confirm={'confirm'} onConfirm={mockConfirm} />);
-//       const confirmCTA = getByTestId('bottom-dialog-action-confirm-button');
-//       fireEvent.click(confirmCTA);
-//       expect(mockConfirm).toHaveBeenCalled();
-//     });
-//
-//     it('Should call on confirm when confirm is given and action-confirm is clicked passing event as param', () => {
-//       const mockConfirm = jest.fn();
-//       const { getByTestId } = render(<ConfirmationToastDialog confirm={'confirm'} onConfirm={mockConfirm} />);
-//       const confirmCTA = getByTestId('bottom-dialog-action-confirm-button');
-//       fireEvent.click(confirmCTA);
-//       expect(mockConfirm).toHaveBeenCalledWith(new MouseEvent('click'));
-//     });
-//   });
-//
-//   describe('secondary actions behavior', () => {
-//     it('Should show action-secondary button when secondary is given', () => {
-//       const { getByTestId } = render(<ConfirmationToastDialog secondary={'secondary'} />);
-//       const secondaryIsShown = getByTestId('bottom-dialog-action-secondary-button').textContent === 'secondary';
-//       expect(secondaryIsShown).toBeTruthy();
-//     });
-//
-//     it('Should NOT show action-secondary button when secondary is NOT given', () => {
-//       const { queryAllByTestId } = render(<ConfirmationToastDialog />);
-//       const secondaryIsNotShown = !queryAllByTestId('bottom-dialog-action-secondary-button').length;
-//       expect(secondaryIsNotShown).toBeTruthy();
-//     });
-//
-//     it('Should call on secondary when confirm is given and action-secondary is clicked', () => {
-//       const mockSecondary = jest.fn();
-//       const { getByTestId } = render(<ConfirmationToastDialog secondary={'secondary'} onSecondary={mockSecondary} />);
-//       const secondaryCTA = getByTestId('bottom-dialog-action-secondary-button');
-//       fireEvent.click(secondaryCTA);
-//       expect(mockSecondary).toHaveBeenCalled();
-//     });
-//
-//     it('Should call on secondary when secondary is given and action-secondary is clicked passing event as param', () => {
-//       const mockSecondary = jest.fn();
-//       const { getByTestId } = render(<ConfirmationToastDialog secondary={'secondary'} onSecondary={mockSecondary} />);
-//       const secondaryCTA = getByTestId('bottom-dialog-action-secondary-button');
-//       fireEvent.click(secondaryCTA);
-//       expect(mockSecondary).toHaveBeenCalledWith(new MouseEvent('click'));
-//     });
-//   });
-// });
