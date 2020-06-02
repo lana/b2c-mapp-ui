43c1f2f121beedb5d74b3028d256ef1c9aa5b07c
Tania Marin <nicte.tania@gmail.com>
2020-06-02 06:25:41 -0500

    Add new sizes to StorybookMobileDeviceSimulator

--- src/components/StorybookMobileDeviceSimulator/StorybookMobileDeviceSimulator.js
+++ src/components/StorybookMobileDeviceSimulator/StorybookMobileDeviceSimulator.js
@@ -5,6 +5,13 @@ const deviceNameLookup = {
   pixel2: 'Pixel 2',
   galaxyS5: 'Galaxy S5',
   iphone5: 'iPhone 5',
+  samsungA30: 'Samsung A30',
+  samsungA50: 'Samsung A50',
+  huaweiMate20Lite: 'Huawei Mate 20 Lite',
+  small: 'Small',
+  medium: 'Medium',
+  large: 'Large',
+  extraLarge: 'Extra Large',
 };
 
 const availableDevices = Object.values(deviceNameLookup);
@@ -25,6 +32,34 @@ const deviceNameToResolutionLookup = {
   [deviceNameLookup.iphone5]: {
     width: '320px',
     height: '568px',
+  },
+  [deviceNameLookup.samsungA30]: {
+    width: '412px',
+    height: '892px',
+  },
+  [deviceNameLookup.samsungA50]: {
+    width: '412px',
+    height: '892px',
+  },
+  [deviceNameLookup.huaweiMate20Lite]: {
+    width: '360px',
+    height: '780px',
+  },
+  [deviceNameLookup.small]: {
+    width: '320px',
+    height: '568px',
+  },
+  [deviceNameLookup.medium]: {
+    width: '360px',
+    height: '640px',
+  },
+  [deviceNameLookup.large]: {
+    width: '360px',
+    height: '780px',
+  },
+  [deviceNameLookup.extraLarge]: {
+    width: '412px',
+    height: '892px',
   },
 };
 
