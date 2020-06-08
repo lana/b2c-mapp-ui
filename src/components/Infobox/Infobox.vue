87204d83110eafdbbba56e0a07a8277f5850564f
Jason Dujardin-Terry <jason.dujardin@lana.xyz>
2020-06-08 11:45:04 +0200

    Added new Infobox component and its Storybook stories

---
+++ src/components/Infobox/Infobox.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="header-info" :data-testid="dataTestId">
+    <TextParagraph class="header-info-text">
+      <slot/>
+    </TextParagraph>
+  </section>
+</template>
+
+<script src="./Infobox.js"/>
+<style lang="scss" scoped src="./Infobox.scss"/>
