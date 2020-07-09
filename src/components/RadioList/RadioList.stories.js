d27919f7588204dc49948c9d6975b38f20011d0f
Jason Dujardin-Terry <jason.dujardin@lana.xyz>
2020-07-09 14:45:04 +0200

    Updated RadioList to accept a new htmlLabel property for an option, which will be rendered with v-html. Bumped the PATCH version in package.json

--- src/components/RadioList/RadioList.stories.js
+++ src/components/RadioList/RadioList.stories.js
@@ -70,8 +70,71 @@ const defaultExample = () => ({
   `,
 });
 
+const withHtmlLabel = () => ({
+  components: {
+    RadioList,
+    StorybookMobileDeviceSimulator,
+  },
+  props: {
+    device: {
+      default: select('Simulated Mobile Device', [...availableDevices], availableDevices[0]),
+    },
+    title: {
+      default: text('Title', 'Example Title'),
+    },
+    disabled: {
+      default: boolean('Is Disabled?', false),
+    },
+  },
+  data() {
+    return {
+      selectedValue: 'option2',
+      options: [
+        {
+          label: 'Option 1',
+          value: 'option1',
+        },
+        {
+          htmlLabel: `
+            <p>Option 2</p>
+            <p>With <strong>fancy</strong>&nbsp;<em>HTML</em></p>
+          `,
+          value: 'option2',
+        },
+        {
+          label: 'Option 3',
+          value: 'option3',
+        },
+      ],
+    };
+  },
+  methods: {
+    onInput: action('Changed!'),
+  },
+  template: `
+    <div style="margin: 10px 50px 10px 50px;">
+      <h2><strong>RadioList:</strong>&nbsp;A control that allows a user to select an option by showing all available options as a list of radio buttons.</h2>
+      <hr>
+      <StorybookMobileDeviceSimulator :device="device">
+        <RadioList v-model="selectedValue"
+                   id="exampleRadioList"
+                   :title="title"
+                   :options="options"
+                   :disabled="disabled"
+                   @input="onInput"
+        />
+        <hr>
+        <div style="margin: 20px">
+          Bound value: {{ selectedValue }}
+        </div>
+      </StorybookMobileDeviceSimulator>
+    </div>
+  `,
+});
+
 export {
   defaultExample,
+  withHtmlLabel,
 };
 
 export default RadioListStories;
