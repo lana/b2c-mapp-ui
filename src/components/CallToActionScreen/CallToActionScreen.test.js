9f2277473d8d4f1d5944a5b3a863a0074f773f5b
José Ortiz <jose.ortiz@lana.xyz>
2021-11-15 18:25:50 -0300

    Fix CallToActionScreen emits property

--- src/components/CallToActionScreen/CallToActionScreen.test.js
+++ src/components/CallToActionScreen/CallToActionScreen.test.js
@@ -15,4 +15,23 @@ describe('CallToActionScreen unit test ', () => {
     const clickEventEmitted = wrapper.emitted().click;
     expect(clickEventEmitted).toBeTruthy();
   });
+
+  it('Should not emit click event when button inside slot is clicked', async () => {
+    const wrapper = mount(
+      CallToActionScreen,
+      {
+        props: { ...defaultProps },
+        slots: {
+          secondaryAction: {
+            methods: { onClick() { return jest.fn(); } },
+            template: '<button data-testid="inner-button" @click="onClick">button</button>',
+          },
+        },
+      },
+    );
+    const button = wrapper.find('button[data-testid="inner-button"]');
+    await button.trigger('click');
+    const clickEventEmitted = wrapper.emitted().click;
+    expect(clickEventEmitted).toBeFalsy();
+  });
 });
